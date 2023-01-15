55dbf024b0b42fa85fb2ed42be66001edba8418c
Damian Woźniak <wizmi2424@gmail.com>
2023-01-15 18:32:33 +0100

    147 adding to cart working

---
+++ MyBook.DataAccess/Repository/IRepository/IShoppingCartRepository.cs
@@ -0,0 +1,13 @@
+﻿using MyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBook.DataAccess.Repository.IRepository
+{
+    public interface IShoppingCartRepository : IRepository<ShoppingCart>
+    {
+    }
+}
---
+++ MyBook.DataAccess/Repository/ShoppingCartRepository.cs
@@ -0,0 +1,21 @@
+﻿using MyBook.DataAccess.Repository.IRepository;
+using MyBook.DataAccess.Data;
+using MyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBook.DataAccess.Repository
+{
+    public class ShoppingCartRepository : Repository<ShoppingCart>, IShoppingCartRepository
+    {
+        private ApplicationDbContext _db;
+
+        public ShoppingCartRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+    }
+}
